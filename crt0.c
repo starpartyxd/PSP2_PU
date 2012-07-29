74b782bbc4ba92eb59ed8bc0f020001df41b4351
Simp <tshomega@gmail.com>
2012-07-28 20:46:13 -0400

    Updated code
    [!] Now uses Linblow's method since it's much much better ( Thanks
    Linblow :) )

--- crt0.c
+++ crt0.c
@@ -4,57 +4,37 @@
 #include <pspthreadman.h>
 #include <pspthreadman_kernel.h>
 
-PSP_MODULE_INFO("PSP2_PU", 0x3008, 1, 2);
+PSP_MODULE_INFO("PSP2_PU", 0x1000, 1, 2);
 PSP_MAIN_THREAD_ATTR(0); 
 
 int module_start(SceSize args, void *argp) __attribute__((alias("_start")));
 int module_stop(SceSize args, void *argp) __attribute__((alias("_stop")));
-
 SceUID thid;
-unsigned int *p0 = (unsigned int) (0x088006f0);
-unsigned int *p1 = (unsigned int) (0x088006f4);
-unsigned int *p2 = (unsigned int) (0x088006f8);
-unsigned int *p3 = (unsigned int) (0x088006fc);
-unsigned int *p4 = (unsigned int) (0x08800700);
-unsigned int *p6 = (unsigned int) (0x08800708);
-unsigned int *p7 = (unsigned int) (0x0880070c);
-unsigned int *p9 = (unsigned int) (0x08800714);
-unsigned int *p10 = (unsigned int) (0x08800718);
-unsigned int *p11 = (unsigned int) (0x0880071c);
-unsigned int *p12 = (unsigned int) (0x08800720);
-unsigned int *p13 = (unsigned int) (0x08800724);
-unsigned int *p14 = (unsigned int) (0x08800728);
-unsigned int *p15 = (unsigned int) (0x0880072c);
-unsigned int *p16 = (unsigned int) (0x08800730);
-unsigned int *p17 = (unsigned int) (0x08800734);
-unsigned int *p18 = (unsigned int) (0x0880004c);
 
 void Function()
 {
   sceKernelDelayThread(15000000);
   while(!sceKernelFindModuleByName("sceKernelLibrary"))
   sceKernelDelayThread(100000);
-
-  // Quick and dirty, just write instructions and patch the hook.
-  // Whatever works, right?
   
-  *p0 = (unsigned int) (0x3c0808e5);
-  *p1 = (unsigned int) (0x8d0861c0);
-  *p2 = (unsigned int) (0x3c0a08c8);
-  *p3 = (unsigned int) (0x354ada24);
-  *p4 = (unsigned int) (0x11000009);
-  *p6 = (unsigned int) (0x21080824);
-  *p7 = (unsigned int) (0x8d080000);
-  *p9 = (unsigned int) (0x11000004);
-  *p10 = (unsigned int) (0x3c0b3410);
-  *p11 = (unsigned int) (0x356b4000);
-  *p12 = (unsigned int) (0xad4b0000);
-  *p13 = (unsigned int) (0x03e00008);
-  *p14 = (unsigned int) (0x3c0c0080);
-  *p15 = (unsigned int) (0x358c8021);
-  *p16 = (unsigned int) (0xad4c0000);
-  *p17 = (unsigned int) (0x03e00008);
-  *p18 = (unsigned int) (0x0a2001bc);
+   unsigned int PATCH[] = {
+        0x3C0808E5,
+        0x8D0861C0,
+        0x3C090080,
+        0x35298021,
+        0x3C0A3410,
+        0x11000004,
+        0x354A4000,
+        0x8D080824,
+        0x55000001,
+        0x01404825,
+        0x3C0808C9,
+        0x03E00008,
+        0xAD09DA24,
+    };
+   
+    memcpy(0x088006F0, PATCH, sizeof(PATCH));
+    *((unsigned int*)0x0880004C) = 0x0A2001BC;
   return;
 }
 

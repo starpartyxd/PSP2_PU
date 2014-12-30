2e310c8ef63bf0946aac0fc48031f5d1603612db
Omega2058 <tshomega@gmail.com>
2014-12-30 01:11:27 -0500

    Fixed freezing issues.

--- crt0.c
+++ crt0.c
@@ -16,98 +16,107 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include <pspkernel.h>
 #include <pspmodulemgr.h>
 #include <pspthreadman.h>
 #include <pspiofilemgr.h>
 
-PSP_MODULE_INFO("PSP2_PU", 0x1000, 1, 1);
+#define BASE 0x08e5d7a0
+#define MISSION_ADDRESS 0x08df7e64
+#define CONTROLLER_ADDRESS 0x08c8da24
+
+#define CONTROLLER_VALUE_OLD 0x00808021
+#define CONTROLLER_VALUE_NEW 0x34104000
+
+PSP_MODULE_INFO("PSP2_PU", 0x1000, 1, 2);
 
 int module_start(SceSize args, void *argp) __attribute__((alias("_start")));
 int module_stop(SceSize args, void *argp) __attribute__((alias("_stop")));
 
 SceUID thid;
 unsigned char gameID[10];
-
-unsigned int PATCH[] = {
-        0x3C0808E5,
-        0x8D0861C0,
-        0x3C090080,
-        0x35298021,
-        0x3C0A3410,
-        0x11000004,
-        0x354A4000,
-        0x8D080824,
-        0x55000001,
-        0x01404825,
-        0x3C0808C9,
-        0x03E00008,
-        0xAD09DA24,
-};
+unsigned int select[] = { 0x0923bcb4, 0x0923a204, 0x0923bca4, 0x0940c630,
+		0x970cefc, 0x097521dc };
 
 /*
  * Verifies that the game is running.
  */
 int findGame(void) {
-	int currAttempts = 0;
 	SceUID fd;
-	
-	do {
-		fd = sceIoOpen("disc0:/UMD_DATA.BIN", PSP_O_RDONLY, 0777);
-		if (currAttempts == 2)
-			break;
-		currAttempts++;
-	} while (fd <= 0);
-	
+
+	fd = sceIoOpen("disc0:/UMD_DATA.BIN", PSP_O_RDONLY, 0777);
 	sceIoRead(fd, gameID, 10);
 	sceIoClose(fd);
-	
-	if(strncmp(gameID, "ULUS-10529", 10) == 0)
-		return 0;
 
-	return -1;
+	if (strncmp(gameID, "ULUS-10529", 10) == 0) {
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+/**
+ * Performs the patch responsible for picking up items.
+ */
+void performPatch() {
+	int i = 0;
+	u32 final = 0;
+
+	if (_lw(MISSION_ADDRESS) == 0) {
+		return;
+	}
+
+	if(_lw(BASE) == 0) {
+		return;
+	}
+
+	final = _lw(BASE) + 0x824;
+
+	if (_lw(final) == 1) {
+		_sw(CONTROLLER_VALUE_NEW, CONTROLLER_ADDRESS);
+	} else {
+		_sw(CONTROLLER_VALUE_OLD, CONTROLLER_ADDRESS);
+	}
 }
 
 /*
  * Main function that does the black magic.
  */
-void o_Function()
-{
+void o_Function() {
+	int status = 0;
+
 	/*
 	 * Wait until it's completely booted.
 	 */
 	sceKernelDelayThread(15000000);
-	while(!sceKernelFindModuleByName("sceKernelLibrary"))
+	while (!sceKernelFindModuleByName("sceKernelLibrary"))
 		sceKernelDelayThread(100000);
-	
+
 	/*
 	 * First we want to confirm that the gameID is correct.
-	 * We copy it into memory and hook to our patch in user space if it is.
 	 */
-	if(findGame() == 0) {
-		memcpy(0x088006F0, PATCH, sizeof(PATCH));
-		*((unsigned int*)0x0880004C) = 0x0A2001BC;
+	if (findGame() == -1) {
+		sceKernelStopUnloadSelfModule(0, NULL, &status, NULL);
+		return;
 	}
-	
-	/*
-	 * Stop and unload the module since we're finished.
-	 */
-	int status;
-	sceKernelStopUnloadSelfModule(0, NULL, &status, NULL);
-	return;
+
+	while (1) {
+		performPatch();
+		sceKernelDelayThread(1000);
+	}
+
 }
 
-int _start(SceSize args, void *argp)
-{
-	thid = sceKernelCreateThread("FunctionThread", &o_Function, 0x18, 0x1000, 0, NULL);
-	if(thid >= 0)
+int _start(SceSize args, void *argp) {
+	thid = sceKernelCreateThread("FunctionThread", &o_Function, 0x18, 0x1000, 0,
+			NULL);
+	if (thid >= 0)
 		sceKernelStartThread(thid, 0, NULL);
 	return 0;
 }
 
-int _stop(SceSize args, void *argp)
-{
- 	sceKernelTerminateThread(thid);
+int _stop(SceSize args, void *argp) {
+	sceKernelTerminateThread(thid);
 	return 0;
 }
